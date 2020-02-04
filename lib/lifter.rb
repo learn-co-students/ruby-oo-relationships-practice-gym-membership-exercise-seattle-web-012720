af27159041310858abdda7e78c4b4ccc53b1c821
github username <github email address>
2020-02-03 17:22:45 -0800

    Done.

--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,37 @@
 class Lifter
   attr_reader :name, :lift_total
+  @@all = []
 
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def memberships
+    Membership.all.select { |membership| membership.lifter == self }
+  end
+
+  def gyms
+    self.memberships.map { |membership| membership.gym }
+  end
+
+  def self.average_lift_total
+    x = self.all.map { |lifter| lifter.lift_total }
+    average = (x.sum.to_f) / (x.length.to_f)
+  end
+
+  def membership_cost_total
+    all_costs = self.memberships.map { |membership| membership.cost }
+    all_costs.sum
+  end
+
+  def new_gym(gym, cost)
+    newest = Membership.new(cost)
+    newest.gym = gym
   end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,8 +1,16 @@
 # You don't need to require any of the files in lib or pry.
 # We've done it for you here.
-require_relative '../config/environment.rb'
+require_relative "../config/environment.rb"
 
-# test code goes here
+Brad = Lifter.new("Brad", 50)
+James = Lifter.new("James", 70)
+Helena = Lifter.new("Helena", 90)
+
+ymca = Gym.new("YMCA")
+core = Gym.new("Core")
+
+mem1 = Membership.new(Brad, ymca, 40)
+mem2 = Membership.new(James, ymca, 50)
 
 binding.pry
 
